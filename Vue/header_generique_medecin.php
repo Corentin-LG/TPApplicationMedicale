f8c3e77909aadc58b12e22475a85bd8afef67b35
Corentin-LG <73930639+Corentin-LG@users.noreply.github.com>
2023-03-18 15:09:36 +0100

    f+h g à display medecin

---
+++ Vue/header_generique_medecin.php
@@ -0,0 +1,65 @@
+<?php
+
+   require('../Controller/Util.php');
+   
+   
+   session_start();
+    /*-- Verification si le formulaire d'authenfication a été bien saisie --*/
+   if($_SESSION["acces"]!='y')
+   {
+            /*-- Redirection vers la page d'authentification --*/
+           header("location:index.php");
+   }
+   else{
+        $Util = new Util();
+        $Utilisateur = $Util->getUtilisateurById($_SESSION["ID_CONNECTED_USER"]);
+        $Medecin = new Secretaire();
+        $Medecin = $Utilisateur->getMedecin();
+   }
+   
+
+    
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>
+               <?php
+                    
+                    echo $Medecin->getNom_Medecin().' '.$Medecin->getPrenom_Medecin();
+               ?>
+        </title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.css" type="text/css" />
+        <link rel="stylesheet" href="js/jquery/css/ui-lightness/jquery-ui-1.9.2.custom.css" type="text/css" />
+        <link rel="shortcut icon" href="bootstrap/img/brain_icon_2.ico"/>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div id="content" class="span9">
+                    <div class="main_body">
+                    
+                        <div class="Home-Header">
+                            <div class="Slogan">
+                                
+                            </div>
+                            <div class="Contact-Research">
+
+                            </div>
+                            <div class="Logo">
+
+                            </div>
+                        </div>
+                        <div class="Horizontal-menu">
+                            <center>
+                                <h4>
+                                    <?php
+                                        echo $Medecin->getNom_Medecin().' '.$Medecin->getPrenom_Medecin();
+                                   ?>
+                                </h4>
+                            </center>
+                        </div>
+
+</html>
+?>
